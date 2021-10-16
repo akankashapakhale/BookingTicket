9466f71aaa687bc0b3dce514ed193d304fb727fa
Shivendra Singh <shiven.dct@gmail.com>
2021-10-16 11:41:52 +0530

    Initial Commit

---
+++ BokingTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BokingTicket
+//
+//  Created by Danish computer technologies on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
